ca630e041b5461a0a4443d25018fa8ec25197a39
vpro <samba.pendela@gmail.com>
2026-07-01 21:41:49 +0530

    pandas completed

--- 06-pandas.py
+++ 06-pandas.py
@@ -1,7 +1,106 @@
 import pandas as pd
 
+
+# Example-10
+# df1 = pd.DataFrame({
+#     "Name" : ["Emp1","Emp2"]
+# })
+
+# df2 = pd.DataFrame({
+#     "Name" : ["Emp3","Emp4"]
+# })
+
+# df3 = pd.concat([df1,df2])
+# print(df3)
+
+# df4 = pd.concat([df1,df2],axis=1)
+# print(df4)
+
+
+
+
+# Example-9
+# df1 = pd.read_csv("one.csv")
+# df1 = pd.DataFrame({
+#     "EmpID":[101,102,103],
+#     "Name":["Emp1","Emp2","Emp3"]
+# })
+
+# df2 = pd.read_csv("two.csv")
+# df2 = pd.DataFrame({
+#     "EmpID":[101,102,103],
+#     "Salary":[50000,60000,70000]
+# })
+
+# df3 = pd.merge(df1,df2,on="EmpID")
+# print(df3)
+
+# df3 = pd.merge(df1,df2,on="EmpID",how="left")
+# print(df3)
+
+# df3 = pd.merge(df1,df2,on="EmpID",how="right")
+# print(df3)
+
+# df3 = pd.merge(df1,df2,on="EmpID",how="inner")
+# print(df3)
+
+# df3 = pd.merge(df1,df2,on="EmpID",how="outer")
+# print(df3)
+
+
+
+
+
+# Example-8
+# employees = {
+#     "EmpID": [101,102,103,104,105],
+#     "Name": ["Sam","John","David","Priya","Anjali"],
+#     "Department": ["IT","HR","Finance","IT","Sales"],
+#     "Salary": [55000,70000,45000,90000,60000],
+#     "Experience": [2,5,1,8,4]
+# }
+# df = pd.DataFrame(employees)
+
+# df1 = df.sort_values("Salary")
+# df2 = df.sort_values("Salary",ascending=False)
+# print(df1)
+# print(df2)
+
+# df1 = df.sort_values(
+#     by=["Department","Salary"],
+#     ascending=[False,True]
+# )
+# print(df1)
+
+# df1 = df.sort_values("Salary",ascending=False).head(3)
+# print(df1)
+
+# df1 = df.groupby("Department")["Salary"].sum()
+# print(df1)
+
+# df1 = df.groupby("Department")["Salary"].mean()
+# print(df1)
+
+# df1 = df.groupby("Department")["Salary"].max()
+# print(df1)
+
+# df1 = df.groupby("Department")["Salary"].min()
+# print(df1)
+
+# df1 = df.groupby("Department")["Salary"].count()
+# print(df1)
+
+# df1 = df.groupby("Department")["Salary"].agg(["min","max","mean","sum","count"])
+# print(df1)
+
+
+
+
+
+
+
 # Example-7
-df = pd.read_csv("employees_null.csv")
+# df = pd.read_csv("employees_null.csv")
 # print(df)
 # print(df.isnull())
 # print(df.isnull().sum())
