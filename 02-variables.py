9e8fd8219b826f830200cd877f2e48158d967a90
VPro <samba.pendela@gmail.com>
2026-07-09 08:56:09 +0530

    variables

--- 02-variables.py
+++ 02-variables.py
@@ -1,12 +1,139 @@
+# Tuple - collection of values, hetrogeneous, Ordered, index-0, (), immutable
+# tuple1 = (10,20,30,40,50)
+# print(tuple1)
+# print(type(tuple1))
+# print(tuple1[0:2])
+    # [10,20,30]
+    # [20,30,40]
+
+# tuple2 = (10,20,30,40,50)
+# tuple2[0] = 1000
+
+
+
+
+# List - collection of values, hetrogeneous, Ordered, index-0, [], mutable
+# list1 = ["VPro",2,True,100.0]
+# print(list1)
+# print(type(list1))
+
+# list2 = [10,50,30,40,20]
+# print(list2[0],list2[-5])
+# print(list2[2],list2[-3])
+# print(list2[0:3])   #[10, 20, 30]
+# print(list2[:2])    #[10, 20]
+# print(list2[2:])    #[30, 40, 50]
+# print(list2[-4:-1]) #[20, 30, 40]
+# print(list2[-3:])   #[30, 40, 50]
+# print(len(list2))
+# print(max(list2))
+# print(min(list2))
+# print(sum(list2))
+# print(sum(list2) / len(list2))
+# list2.sort()
+# print(list2)
+
+# list2 = [10,20,30,40,50]
+# list2[0] = 100
+# print(list2)
+
+
+
+
+
+
+
+
 # integer - numbers (int,float)
-num1 = 200
-num2 = 100
-addition = num1 + num2
-print(f"Result :{addition}")
+# num1 = 200
+# num2 = 100
+# addition = num1 + num2
+# print(f"Result :{addition}")
+
+# num1 = 0x123ABC
+# print(num1)
+
+# num2 = 0o123
+# print(num2)
+
+# num3 = 0b101010
+# print(num3)
 
 
+# print(10 % 3)   # Reminder : 1
+# print(2 ** 3)   # power : 8
+# print(10 // 2)  # Floor Division : 5
+# print(10 / 2)   # Division : 5.0
+# print(20 / 3)
+
+# num1 = 123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789
+# print(num1)
 
 
+# num1 = 10.123
+# print(num1)
+# print(type(num1))
+
+
+# num1 = 99.12345
+# print(round(num1,2))
+# print(round(num1,3))
+# print(round(num1,6))
+# print(round(num1,0))
+
+# print(f"{num1:.2f}")
+# print(f"{num1:.4f}")
+
+# print(format(num1,".2f"))
+# print(format(num1,".4f"))
+
+# num1 = 0.1
+# num2 = 0.2
+# num3 = num1 + num2  
+# print(num3) # 0.30000000000000004
+
+# == (comapres values)
+# print(10 == 10)
+# print(0.1+0.2 == 0.3)
+# print(0.1+0.2 == 0.30000000000000004)
+
+# Boolean
+# True - 1 & False - 0
+
+# flag = True
+# flag1 = False
+# print(flag)
+# print(flag1)
+# print(type(flag))
+
+# print(True + True)
+# print(True + 1)
+# print(True + False + 1 + 0 + True)
+# print(False / True)
+# print(True / False) #ZeroDivisionError: division by zero
+# print("1" + True) # TypeError: can only concatenate str (not "bool") to str
+
+# Falsy Values : False, None, 0, 0.0, "", [], {}, (), set()
+# Truthy Values : True,       1, 100.1, "Hello", [10], {"num1":10}, (10,)
+
+
+# num1 = 25.99
+# print(int(num1))
+
+# num2 = "25"
+# print(int(num2))
+
+# print(int(True))
+# print(int(False))
+
+# num1 = 25
+# print(float(num1))
+
+# num2 = "25.0"
+# print(float(num2))
+
+# x = 10
+# print(x.bit_length())
 
 
 # string - collection of characters
