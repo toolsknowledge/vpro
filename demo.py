fda5350ed39d7f28b269ad26fda60f359f2d1f11
VPro <samba.vpro@gmail.com>
2026-07-22 20:49:15 +0530

    variables started

---
+++ demo.py
@@ -0,0 +1,49 @@
+import keyword
+print(keyword.kwlist)
+
+num1 = 100
+print(num1)
+
+num2 = -100
+print(num2)
+
+num3 = 0
+print(num3)
+
+# num4 = 0100
+
+num5 = 10_000
+print(num5)
+
+print(True + True)
+print(1 + True + False)
+
+num6 = 123456789123456789123456789123456789123456789123456789123456789
+print(num6)
+
+num7 = 0x123ABC
+print(num7)
+
+num8 = 0o123
+print(num8)
+
+num9 = 0b101010
+print(num9)
+
+num10 = "10"
+print(type(num10))
+x = int(num10)
+print(type(x))
+
+num11 = "10.5" 
+y = float(num11)
+z = int(y)
+print(z)
+
+print(int(True))
+print(int(False))
+
+print(float(True))
+print(float(False))
+
+print(int("Hello"))
