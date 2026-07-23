2086d243ad5906580624d46992e8561f30c3865e
VPro <samba.vpro@gmail.com>
2026-07-23 20:55:10 +0530

    string datatype completed

--- demo.py
+++ demo.py
@@ -1,49 +1,108 @@
-import keyword
-print(keyword.kwlist)
+# sub = "Python"
+# print(sub)
 
-num1 = 100
-print(num1)
+# sub1 = 'Deep Learning !!!'
+# print(sub1)
 
-num2 = -100
-print(num2)
+# sub2 = """
+#         in upcoming companies looking for
+#         Machine Learning with Quantum Computing !!!
+# """
+# print(sub2)
 
-num3 = 0
-print(num3)
+# msg = "Hello"
+# print(msg[0],msg[-5])
+# print(msg[4],msg[-1])
+# print(msg[::-1])
+# print(msg[::-2])
+# print(msg[::-3])
+# print(msg[0:2])
+# print(msg[-3:])
 
-# num4 = 0100
+# print("Hello \n Python")
+# print("Hello \t Python")
+# print("Hello how \"are\" you")
+# path = r"C:\Users\Admin"
+# print(path)
 
-num5 = 10_000
-print(num5)
+# print("Python " * 20)
+# print("Hello" + " Python")
+# print("10" + "20")
+# print( int("10") + int("20") )
+# print("Hello " + 20) # Err
 
-print(True + True)
-print(1 + True + False)
 
-num6 = 123456789123456789123456789123456789123456789123456789123456789
-print(num6)
+# name = "VPro"
+# sub = "Agentic AI"
+# print(f"{name} Started {sub}")
 
-num7 = 0x123ABC
-print(num7)
+# sub1 = "Quantum Computing"
+# version = 1.0
+# print(f"{name} will start {sub1} with {version} version soon !!!")
 
-num8 = 0o123
-print(num8)
 
-num9 = 0b101010
-print(num9)
+# name = "Samba"
+# age = 40
+# print("my name is {} and age is {}".format(name,age))
+# print("my name is {1} and age is {0}".format(age,name))
 
-num10 = "10"
-print(type(num10))
-x = int(num10)
-print(type(x))
 
-num11 = "10.5" 
-y = float(num11)
-z = int(y)
-print(z)
 
-print(int(True))
-print(int(False))
 
-print(float(True))
-print(float(False))
 
-print(int("Hello"))
+
+
+
+
+
+
+
+# import keyword
+# print(keyword.kwlist)
+
+# num1 = 100
+# print(num1)
+
+# num2 = -100
+# print(num2)
+
+# num3 = 0
+# print(num3)
+
+# # num4 = 0100
+
+# num5 = 10_000
+# print(num5)
+
+# print(True + True)
+# print(1 + True + False)
+
+# num6 = 123456789123456789123456789123456789123456789123456789123456789
+# print(num6)
+
+# num7 = 0x123ABC
+# print(num7)
+
+# num8 = 0o123
+# print(num8)
+
+# num9 = 0b101010
+# print(num9)
+
+# num10 = "10"
+# print(type(num10))
+# x = int(num10)
+# print(type(x))
+
+# num11 = "10.5" 
+# y = float(num11)
+# z = int(y)
+# print(z)
+
+# print(int(True))
+# print(int(False))
+
+# print(float(True))
+# print(float(False))
+
+# # print(int("Hello"))
