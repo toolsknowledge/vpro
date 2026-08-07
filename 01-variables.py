0a980d874291ad245c1b12d6b7907f3f56cb0528
VPro <samba.vpro@gmail.com>
2026-08-07 07:50:09 +0530

    variables started

---
+++ 01-variables.py
@@ -0,0 +1,58 @@
+# Varibables are used to "store" the data
+# Ex. Integer(Ex. 100), String (Ex. "Hello") Ex. Boolean (True / False) Ex. list Ex.[]  , tuple Ex.()....... 
+# Rules to declare Variables
+# Rule 1. declaration must contain a-z,A-Z,0-9,$ and _
+# Rule 2. declaration never starts with "digits"
+# Rule 3. never use "predefined keywords" as "variables declaration"
+
+# String
+# collection of "characters"
+# 1) "" (double quotes).  2) '' (single quotes).  3) """ """ (tripple quotes)
+# """ """ (tripple quotes), used to declare the paragraphs
+
+# wish = "Hello"
+# print(wish)
+
+# msg = 'Python'
+# print(msg[0],msg[-6])
+# print(msg[0:2])
+# print(msg[:2])
+# print(msg[4:])
+# print(msg[-2:])
+# print(msg[::-1])
+
+# content = """
+#         1) Python
+#         2) ML (10+)
+#         3) DL (Image Processing)
+#         4) NLP (ChatBot)
+#         5) Gen AI (Tools)
+#         6) Agentic AI
+#         7) Cloud Integrations
+# """
+# print(content)
+
+# x = "Hello"   #(immutable)
+# x[0] = "h"
+# print("h"+x[1:])
+
+# msg = "Gen AI"
+# MSG = "AGENTIC AI"
+# Msg = "Quantum"
+# print(msg,MSG,Msg)
+
+# name = "emp1"
+# age = 40
+# print(f"Employee Name is {name} and Age is {age}")
+# print("Employee Age  {} and his name is {}".format(age,name))
+
+# Integer
+num1 = 200
+num2 = 100
+add = num1 + num2
+print(f"Addition is {add}")
+sub = num1 - num2
+print("Subtraction : {}".format(sub))
+
+
+
