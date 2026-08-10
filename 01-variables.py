0388e3ddec51862f9d8aeaec23d8ae1ebcc7fc65
VPro <samba.vpro@gmail.com>
2026-08-10 08:07:18 +0530

    code

--- 01-variables.py
+++ 01-variables.py
@@ -47,12 +47,129 @@
 # print("Employee Age  {} and his name is {}".format(age,name))
 
 # Integer
-num1 = 200
-num2 = 100
-add = num1 + num2
-print(f"Addition is {add}")
-sub = num1 - num2
-print("Subtraction : {}".format(sub))
+# num1 = 200
+# num2 = 100
+# add = num1 + num2
+# print(f"Addition is {add}")
+# sub = num1 - num2
+# print("Subtraction : {}".format(sub))
+
+# int
+# numbers (positive numbers / negative numbers / zeros)
+# immutable (we can't modify)
+# unlimited size
+
+# Example-1
+# num1 = 100
+# num2 = -100
+# num3 = 0
+# print(num1)
+# print(num2)
+# print(num3)
+# print(type(num1))
+
+# Example-2
+# num1 = 123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789
+# print(num1)
+
+# num2 = 2 ** 1000
+# print(num2)
+
+# Example_3
+# num1 = 1_23_456_78
+# print(num1)
+
+# x = 1_00
+# y = 2_00
+# z = x + y
+# print(z)
+
+# num2 = _100  # Ex. Err
+
+
+# Example-4
+# num1 = 0B11001
+# print(num1)
+
+# num2 = 0O31
+# print(num2)
+
+# num3 = 0X19
+# print(num3)
+
+# Example-5
+# num1 = 25
+# print(bin(num1))
+# print(oct(num1))
+# print(hex(num1))
+
+
+# Example-6 (ASCII)
+# print(ord('A'))
+# print(ord('a'))
+# print(chr(65))
+# print(chr(97))
+
+
+# Example-7 (True - 1 / False - 0)
+# In Python "Booleans are Integers"
+
+# print(True + True)
+# print(True + False + 1)
+# print(True / False)   # Err.ZeroDivisionError
+# print(False / True)
+# print("1" + True)   # Err
+# print("1" + 1) # Err
+
+
+# Example-9
+# == (compare values)
+# is (objects)
+# print(1 == True)
+# print(0 == False)
+
+# print(1 is True)
+# print(0 is False)
+
+
+# Example-10
+# print(abs(-100))
+# print(2 ** 2)
+# Floor Division (rounded)
+# print( 10 // 3 )    
+# Division
+# print( 10 / 3 )
+# Reminder (perventages)
+# print(10%3 ) 
+
+# Example - 11
+# & (and) both should be true
+# | (or) either one should be true
+# ^ (xor) both should be different
+
+# print( True and True )
+# print( True | False )
+# print( False | True )
+# print( True ^ False )
+# print( True ^ True )
+
+# Example-12
+# num1 = 0.1         
+# num2 = 0.2
+# num3 = num1 + num2      # 0.30000000000000004
+# print(num3)
+
+# print(0.1 + 0.2 == 0.3 )
+
+
+
+
+
+
+
+
+
+
 
 
 
