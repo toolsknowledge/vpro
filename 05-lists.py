36b8a3da61a9cc7de0e764c485eb7999f339cb23
VPro <samba.vpro@gmail.com>
2026-08-11 20:49:28 +0530

    code

--- 05-lists.py
+++ 05-lists.py
@@ -82,3 +82,45 @@
 # Faq-2
 # list1 = [10,20,30]
 # print(list1 * 3)
+
+# list1 = [1,2,3,1,1,2,4,3]
+# print(list1.count(1))
+# print(list1.count(2))
+# print(list1.count(5))
+
+
+# list1 = [1,2,3,1,1,2,4,3]
+# print( list( set( [x for x in list1 if list1.count(x)>1] )) )
+
+# nums = [10,50,20,40,30]
+# print(max(nums))
+# print( sorted(nums)[-2] )
+
+
+# res =  [[] for _ in range(3)] 
+# res[0].append(10)
+# print(res)
+
+# list1 = [100,200,300,400,500]
+# for index,element in enumerate(list1):
+#     print(index,element)
+
+# list1 = [1,2,3,4,5]
+# list2 = ["Python","ML","DL","NLP","GenAI"]
+# for element1,element2 in zip(list1,list2):
+#     print(element1,element2)
+
+
+# print( 
+#     list( map(lambda num1:num1*100,[1,2,3,4,5]) ) 
+#     )
+
+# print( tuple( filter(lambda num1:num1>=300,(100,200,300,400,500)) ) )
+
+# from functools import reduce
+# res = reduce(lambda num1,num2:num1+num2,(1,2,3,4,5))
+# res1 = (res,)
+# print(res1)
+ 
+
+
