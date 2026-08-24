93dd77e0e02116e7301f173a372dd4f3b249d836
VPro <samba.pendela@gmail.com>
2026-08-24 20:26:19 +0530

    pie chart

--- 10-graphs.py
+++ 10-graphs.py
@@ -46,54 +46,88 @@
 # plt.show()
 
 # Bar Chart
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# students = ["Std1","Std2","Std3"]
+
+# python_marks = [85,90,70]
+# java_marks = [78,85,72]
+# sql_marks= [82,88,75]
+# react_marks = [80,92,68]
+
+# x = np.arange(len(students)) # [0,1,2]
+
+# width = 0.2
+
+# plt.figure(figsize=(12,6))
+
+# plt.bar(x - 1.5 * width,        # [0,1,2] - 0.3 = [-0.3,0.7,1.7]
+#         python_marks,
+#         width,
+#         label="Python")
+
+# plt.bar(x - 0.5 * width,        # [0,1,2] - 0.1 = [-0.1,0.9,1.9]
+#         java_marks,
+#         width,
+#         label="java")
+
+# plt.bar(x + 0.5 * width,
+#         sql_marks,
+#         width,
+#         label="sql")
+
+# plt.bar(x + 1.5 * width,
+#         react_marks,
+#         width,
+#         label="react")
+
+# plt.xticks(x,students)
+
+# plt.xlabel("Students")
+
+# plt.ylabel("Marks")
+
+# plt.title("Subject Wise Bar Chart")
+
+# plt.ylim(0,100)
+
+# plt.grid()
+
+# plt.legend()
+
+# plt.show()
+
+# Pie Chart
 import matplotlib.pyplot as plt
-import numpy as np
-
-students = ["Std1","Std2","Std3"]
-
-python_marks = [85,90,70]
-java_marks = [78,85,72]
-sql_marks= [82,88,75]
-react_marks = [80,92,68]
-
-x = np.arange(len(students)) # [0,1,2]
-
-width = 0.2
-
-plt.figure(figsize=(12,6))
-
-plt.bar(x - 1.5 * width,        # [0,1,2] - 0.3 = [-0.3,0.7,1.7]
-        python_marks,
-        width,
-        label="Python")
-
-plt.bar(x - 0.5 * width,        # [0,1,2] - 0.1 = [-0.1,0.9,1.9]
-        java_marks,
-        width,
-        label="java")
-
-plt.bar(x + 0.5 * width,
-        sql_marks,
-        width,
-        label="sql")
-
-plt.bar(x + 1.5 * width,
-        react_marks,
-        width,
-        label="react")
-
-plt.xticks(x,students)
-
-plt.xlabel("Students")
-
-plt.ylabel("Marks")
-
-plt.title("Subject Wise Bar Chart")
-
-plt.ylim(0,100)
-
-plt.grid()
-
-plt.legend()
-
+subjects = ["Python","Java","React","SQL","AWS"]
+marks = [95,85,70,60,90]
+colors = ['gold','skyblue','lightgreen','green','pink']
+explode = (0.1,0,0,0,0)
+plt.figure(figsize=(8,8))
+plt.pie(marks,
+        labels=subjects,
+        colors=colors,
+        autopct='%1.1f%%',
+        startangle=90,
+        shadow=True,
+        counterclock=True,
+        radius=1.0,
+        pctdistance=0.6,
+        labeldistance=0.8,
+        explode=explode,
+        wedgeprops={
+            'edgecolor':'black',
+            'linewidth':2
+        },
+        textprops={
+            'fontsize':12,
+            'color':'black'
+        })
+plt.title("Student Marks Distribution",
+          fontsize=18,
+          fontweight='bold')
+plt.legend(title='Subjects',
+           loc='upper right')
+plt.savefig("marks.png")
 plt.show()
