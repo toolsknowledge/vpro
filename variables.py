04625f5fbe0097a7a206f033b55e51772aed2a78
VPro <samba.pendela@gmail.com>
2026-08-25 06:54:59 +0530

    string and number

--- variables.py
+++ variables.py
@@ -1,24 +1,133 @@
 # msg = "Python"
 # print(msg)
 
-msg = "Hello"   #olH
-print(msg[0])
-print(msg[-5])
+# msg = "Hello"   #olH
+# print(msg[0])
+# print(msg[-5])
 
-print(msg[4])
-print(msg[-4])
+# print(msg[4])
+# print(msg[-4])
 
-print(msg[0:2])
-print(msg[1:3]) # 1 and 2 included. and 3 excluded
-print(msg[2:])
-print(msg[:3])  # 0 - included and 3 - excluded
+# print(msg[0:2])
+# print(msg[1:3]) # 1 and 2 included. and 3 excluded
+# print(msg[2:])
+# print(msg[:3])  # 0 - included and 3 - excluded
 
-print(msg[-3:])
-print(msg[-5:-3])   # "-5, -4" included and "-3 excluded"
+# print(msg[-3:])
+# print(msg[-5:-3])   # "-5, -4" included and "-3 excluded"
 
-print(msg[::-1])
-print(msg[::-2])
-print(msg[::-3]) # oe
+# print(msg[::-1])
+# print(msg[::-2])
+# print(msg[::-3]) # oe
+
+# print("Hello" * 3)
+# msg = """
+#     we will cover
+#     1) Python Basics including libraries
+#     2) ML + QC
+#     3) DL
+#     4) NLP
+#     5) GenAI
+#     6) AgenticAI
+#     7) Cloud Deployment (AWS,AZURE,GCP)
+# """
+# print(msg)
+
+# name = "VPro Skills"
+# print(f"Welcome to {name}")
+
+# emp_name = "Emp1"
+# age = 40
+# print(f"Employee name is {name} and his age is {age}")
+
+# name = "VPro"
+# name = "VPro Skills"
+# print(name)
+
+
+# name = "Employee1"
+# dept = "R & D"
+# print("Employee name is {} and related to {}".format(name,dept))
+
+
+# cmp = "VPro Skills"
+# dept = "Quantum Computing"
+# print("{0} department availble in {1}".format(dept,cmp))
+
+
+# print("Hello \n World")
+# print("Age is \t 40")
+# print("C:\\ProgramFiles\\demo.py")
+# print('It\'s Python')
+# print("I Said \"Good Morning !!!\" ")
+
+
+# strings are (immutable)
+
+# name = "vPro"       # VPro
+# name[0] = "V"
+# print(name)
+
+# name = "vPro"
+# print("V" + name[1:])
+
+
+
+# int
+# positive, negative,zeros
+# num1 = 200
+# num2 = 100
+# add = num1 + num2
+# print(f"Addition is {add}")
+
+
+# num1 = 10
+# res = num1 ** 2
+# print(res)
+
+
+# num1 = 123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789
+# print(num1)
+
+# num1 = 1_0_0
+# num2 = 1_0_0_0
+# print(num1 + num2)
+
+# num1 = 0x123ABC
+# print(num1)
+
+# num2 = 0o123
+# print(num2)
+
+# num3 = 0b101010
+# print(num3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
