af161b1be99f683997dcc1841d8d59f701f46d2d
VPro <samba.pendela@gmail.com>
2026-09-03 06:58:31 +0530

    oops

---
+++ 03-oops.py
@@ -0,0 +1,99 @@
+"""
+    collection of variables and functions called as class
+    "class" is the keyword, used to define the classes
+    "pass" is the keyword, used to define empty class
+    we are able to create object to "classes"
+
+    instance:
+    ********
+        multiple copies will create internally(memory)(ram)
+        one object changes wont effect to any other objects
+        self is used to define instance members
+"""
+
+# class Test:
+#     pass
+
+# obj1 = Test()
+# print(id(obj1))
+
+
+# class Test:
+#     num1 = 200
+
+# obj1 = Test()
+# obj1.num1 = 2000
+# x = obj1.num1
+# print(x)
+
+# obj2 = Test()
+# y = obj2.num1
+# print(y)
+
+# class Test:
+#     num1 = 200
+#     num2 = 100
+
+# obj1 = Test()
+# x = obj1.num1
+# y = obj1.num2
+# add = x + y
+# print(f"Addition :{add}")
+
+
+# class Test:
+#     def __init__(self,param1,param2):
+#         self.num1 = param1
+#         self.num2 = param2
+
+# obj1 = Test(200,100)
+# x = obj1.num1
+# y = obj1.num2
+# print(x+y)
+
+
+# class Test:
+#     def __init__(vpro,msg):
+#         vpro.wish = msg
+
+# obj1 = Test("Hello")
+# x = obj1.wish
+# print(x)
+
+
+# class Test:
+#     def add1(self):
+#         num1 = 200
+#         num2 = 100 
+#         res = num1 + num2
+#         print(res)
+#     def add2(self,param1,param2):
+#         res = param1 + param2
+#         return res
+#     # no para with return
+#     # with para - no return 
+
+# obj1 = Test()
+# obj1.add1()
+# x = obj1.add2(200,100)
+# print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
