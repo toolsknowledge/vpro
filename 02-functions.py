c705557e9637b9fd8b841e155351cc2768296c6e
Samba <samba.pendela@gmail.com>
2026-09-17 21:38:43 +0530

    code

---
+++ 02-functions.py
@@ -0,0 +1,70 @@
+"""
+    business logic
+        (or)
+    set of statements also called as function
+
+    functions are used to "reuse" the business logic
+
+    "def" is the keyword, used to declare the functions
+
+    "pass" is the keyword, used to create empty function   
+"""
+
+# function definition
+# no parameters - no return 
+# def addition():
+#     num1 = 200
+#     num2 = 100
+#     res = num1 + num2
+#     print(f"Addition : {res}")
+
+# function calling
+# addition()
+
+
+# no parameters - with return
+# def addition():
+#     num1 = 200
+#     num2 = 100
+#     res = num1 + num2
+#     return res
+
+
+# x = addition()
+# print(f"Addition : {x}")
+
+
+# with parameters - no return 
+# def addition(num1,num2):
+#     res = num1 + num2
+#     print(f"Addition : {res}")
+
+# addition(200,100)
+
+# with parameters - with return
+# def addition(num1,num2):
+#     res = num1 + num2
+#     return res
+
+# x = addition(200,100)
+# print(f"Addition : {x}")
+
+
+# def my_func(*param1):
+#     print(param1)
+
+# my_func(100,200,300,400,500)
+
+
+# def my_func(*param1,*param2):
+
+
+
+
+
+
+
+
+
+
+
