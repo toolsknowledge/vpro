c4ef6985b75492f6968f82bcce2ac475b6813f74
Samba <samba.pendela@gmail.com>
2026-09-18 07:49:28 +0530

    code

--- 05-graphs.py
+++ 05-graphs.py
@@ -1,3 +1,118 @@
+# heatmap
+# python std1 - 90(dark green). std2 - 60(green). std3 - 40(light green)
+# import matplotlib.pyplot as plt
+# students = ["Std1","Std2","Std3","Std4"]
+# subjects = ["Python","Java","SQL","React"]
+# marks = [[80,70,85,75],
+#          [90,85,88,92],
+#          [60,65,70,68],
+#          [85,90,92,88]]
+# plt.imshow(marks,cmap="plasma")
+# plt.xticks(range(len(subjects)),subjects)
+# plt.yticks(range(len(students)),students)
+# plt.colorbar(label="marks")
+# plt.title("Marks Distribution")
+# plt.savefig("heat.png")
+# plt.show()
+
+
+
+
+
+# box-plot (analysis 1st quarter, half year, 3rd querter,.....)
+# import matplotlib.pyplot as plt
+
+# python_marks = [45,50,52,55,58,60,65,68,70,72]
+# java_marks = [40,48,50,55,60,62,65,70,75,90]
+# sql_marks = [50,55,58,60,62,65,68,70,72,95]
+
+# plt.boxplot([python_marks,java_marks,sql_marks])
+# plt.xticks([1,2,3],["Python","Java","SQL"])
+# plt.title("Subject wise marks distribution")
+# plt.ylabel("Marks")
+# plt.grid(axis='y',linestyle="--",alpha=0.5)
+# plt.savefig("box.png")
+# plt.show()
+
+
+
+
+
+# # pie-chart
+# import matplotlib.pyplot as plt
+
+# expenses = [30,40,15,10,5]
+# categories = ["Rent","Salaries","Marketing","Food","Other"]
+# colors = ["red","blue","green","orange","purple"]
+# explode = [0,0.1,0,0,0]
+# plt.pie(expenses,labels=categories,colors=colors,explode=explode,startangle=90,autopct='%1.1f%%')
+# plt.title("Demo Pie Chart")
+# plt.legend()
+# plt.savefig("pie.png")
+# plt.show()
+
+
+
+# # histogram (categorize the data)
+# import matplotlib.pyplot as plt
+# marks = [45,50,52,55,58,
+#          60,62,65,67,68,
+#          70,72,75,78,80,
+#          82,85,88,90,92]
+# # bins - 5
+# # 92 - 45 = 47 / 5 = 9
+# # 45 - 54
+# # 54 - 63
+# # 63 - 72
+# # 72 - 81
+# # 81 - last
+# plt.hist(marks, bins=5)
+# plt.title("Students Marks Distribution")
+# plt.xlabel("Marks")
+# plt.ylabel("Number of Students")
+# plt.grid(axis='y',linestyle="--",alpha=0.5)
+# plt.savefig("histo.png")
+# plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+# Subplots
+# import matplotlib.pyplot as plt
+
+# plt.subplot(2,2,1)
+# plt.plot([1,2,3],[10,20,30])
+# plt.xlabel("Hours")
+# plt.ylabel("Marks")
+# plt.title("Line Plot")
+
+# plt.subplot(2,2,2)
+# # bar chart
+# plt.bar([1,2,3],[10,20,30])
+# plt.xlabel("Hours")
+# plt.ylabel("Marks")
+# plt.title("Line Plot")
+
+# # scatter plot
+
+# # multi line plot
+
+# plt.tight_layout()
+# plt.show()
+
+
+
+
+
+
 
 
 
