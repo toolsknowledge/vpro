fbe22c850e7aa4619784cfa9d42bbccff96717e3
Samba <samba.pendela@gmail.com>
2026-09-22 06:02:15 +0530

    code

---
+++ 03-lists.py
@@ -0,0 +1,122 @@
+"""
+    collection of "hetrogeneous" and "indexed" elements called as "list"
+    we will represent list with the help of either [] / list()
+    list is mutable
+"""
+
+# list1 = [10,50,20,40,30]
+# print(max(list1))
+# print(min(list1))
+# print(len(list1))
+# print(sum(list1))
+# print(sum(list1) / len(list1))
+
+# res = sorted(list1)
+# print(res)          # [10, 20, 30, 40, 50]
+# print(list1)        # [10, 50, 20, 40, 30]
+
+
+# list1 = ["Python","ML","DL","NLP","GenAI","AgenticAI"]
+# print(list1[0])
+# print(list1[-1])
+# print(list1[:2])        # 0 included and 2 excluded
+# print(list1[-2:])
+# print(list1[2:5])       # 2 included and 5 excluded
+# print(list1[-4:-1])
+# print(list1[::-1])
+
+
+# list1 = [10,20,30,40]
+# list1.append(60)
+# print(list1)            #[10,20,30,40,60]
+
+# list1.insert(4,50)
+# print(list1)            #[10,20,30,40,50,60]
+
+# list2 = [70,80]
+# list1.extend(list2)
+# print(list1)            #[10, 20, 30, 40, 50, 60, 70, 80]
+
+
+# list1.append(10)
+# list1.append(20)
+# print(list1)            #[10, 20, 30, 40, 50, 60, 70, 80, 10, 20]
+
+
+# print(list1.count(10))      #2
+# print(list1.count(50))      #1
+# print(list1.count(100))     #0
+# print(list1.index(10))      #0
+# print(list1.index(20))      #1
+
+# list1.remove(10)
+# print(list1)        #[20, 30, 40, 50, 60, 70, 80, 10, 20]
+
+# list1.remove(40)
+# print(list1)        #[20, 30, 50, 60, 70, 80, 10, 20]
+
+# list1.remove(20)
+# print(list1)        #[30, 50, 60, 70, 80, 10, 20]
+
+# x = list1.pop()
+# print(x)            # 20
+# print(list1)        # [30, 50, 60, 70, 80, 10]
+
+# y = list1.pop()
+# print(y)            # 10
+# print(list1)        # [30, 50, 60, 70, 80]
+
+
+
+# list1 = [10,20,30,40,50]
+# x = list1.pop(1)
+# print(x)
+# print(list1)
+
+
+# list1 = [10,20,30,40,50]
+# list1.clear()
+# print(list1)
+
+# list1 = [10,20,30,40,50]
+# del list1[1]
+# print(list1)
+
+
+# list1 = [10,50,20,40,30]
+# list1.sort()
+# print(list1)
+# sorted() & sort()
+# sorted() - creates the new list
+# sort() - wont creates the new list
+
+# list1.sort(reverse=True)
+# print(list1)
+
+
+# list1 = [10,20,30,40,50]
+
+# res1 = list1[::-1]
+# print(res1)
+
+# list1.reverse()
+# print(list1)
+
+
+# list1 = [10,20,30,40,50]
+# list1[2] = 3000
+# print(list1)        # mutable
+
+
+list1 = [10,20,30,40,50]
+list2 = [100,200,300,400,500]
+
+# for element in list1:
+#     print(element)
+
+# for index,element in enumerate(list1):
+#     print(index,element)
+
+# for element1,element2 in zip(list1,list2):
+#     print(element1,element2)
+
