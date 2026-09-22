08db2b88f17ca89e94175c0d25225f64be7e8009
Samba <samba.pendela@gmail.com>
2026-09-23 05:03:44 +0530

    code

--- demo.py
+++ demo.py
@@ -1,3 +1,77 @@
+# 4 videos - 1
+
+
+# chair = None
+# print(chair)
+
+
+# set
+# set, never allows "duplicates"
+# s1 = {10,20,30,10,20,40}
+# print(s1)
+
+
+# dictionary
+# d1 = {
+#     "key1" : "Hello",
+#     "key2" : "Welcome"
+# }
+# print(d1["key1"])
+# print(d1["key2"])
+# print(d1.keys())
+# print(d1.values())
+# print(d1.items())
+
+
+
+
+# tuple1 = (10,20,30,40,50)     # () / tuple()
+# print(tuple1[0:3])      #0,1,2 -- included. and 3 onwards excluded
+# print(tuple1[2:4])
+# tuple1[0] = 1000
+
+
+# print(tuple1[0])
+# print(tuple1[-4])
+# print(tuple1[2])
+# print(tuple1[-2])
+# print(tuple1[4])
+
+
+
+
+
+
+
+
+
+# list1 = [10,20,30,40,50]
+# print(list1[0])
+# print(list1[-5])
+# print(list1[4])
+# print(list1[2])
+# print(list1[0:2])
+# print(list1[:3])        #0:3
+# print(list1[1:4])       # index "1" included and index "4" excluded
+# print(list1[::2])
+# print(list1[::3])
+# print(list1[::-1])      # reversing
+# print(list1[::-2])      # skip single element [50,30,10]
+
+
+# list1 = [10,20,30,40,50]
+# list1[0] = 1000
+# print(list1)
+
+
+
+
+
+
+
+
+
+
 """
     "VPro" --  String 
     100 - int
@@ -87,5 +161,5 @@
 # print(s1)
 
 # None (Empty / Blank)
-chair = None
-print(chair)
+# chair = None
+# print(chair)
